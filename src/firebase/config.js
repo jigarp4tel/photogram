575b8c7419013a00f0cfa93eea6d8fb100775903
Jigar Patel <44246251+jigarp4tel@users.noreply.github.com>
2021-07-31 04:35:23 -0500

    -initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase/app'
+
+// Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDMD3tOlrvSA9yM4kzT5kQl-O3XWlJRyt8",
+    authDomain: "jigarp4tel-photogram.firebaseapp.com",
+    projectId: "jigarp4tel-photogram",
+    storageBucket: "jigarp4tel-photogram.appspot.com",
+    messagingSenderId: "592904815918",
+    appId: "1:592904815918:web:738b8cd1dc2d3aca5566bf"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
