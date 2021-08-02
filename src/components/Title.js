55942d3961d63aabc912278062420a313abfc1f0
Jigar Patel <44246251+jigarp4tel@users.noreply.github.com>
2021-08-02 01:25:45 -0500

    -updated styles

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from '../assets/img/logo.png'
 
 const Title = () => {
     return (
         <div className="title">
-            <h1>PhotoGram</h1>
+            <img className="logo" src={logo} alt="Logo" />
             <h2>Your Pictures</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
